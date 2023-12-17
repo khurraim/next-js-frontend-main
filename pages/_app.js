4427d6fd8084bc710eb61262c8248f6ae3b02b58
khurraim <khurraim.bcs46.m@gmail.com>
2023-12-18 00:14:21 +0500

    Extra Code Removed

--- pages/_app.js
+++ pages/_app.js
@@ -34,15 +34,8 @@ export default function App({ Component, pageProps }) {
       import('@/assets/vendor/bootstrap/css/bootstrap.min.css');
       import('@/assets/vendor/fonts/circular-std/style.css');
       import('@/assets/libs/css/style.css');
-      // import('@/assets/vendor/fonts/fontawesome/css/fontawesome-all.css');
-      //import('@/assets/vendor/charts/chartist-bundle/chartist.css');
-      //import('@/assets/vendor/charts/morris-bundle/morris.css');
-      // import('@/assets/vendor/fonts/material-design-iconic-font/css/materialdesignicons.min.css');
-      //import('@/assets/vendor/charts/c3charts/c3.css');
       import('@/assets/vendor/fonts/flag-icon-css/flag-icon.min.css');
     }
-
-    
 
   }, [router.pathname]);
   return <><Component {...pageProps} /><ToastContainer /> </>

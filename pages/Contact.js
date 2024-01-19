e544111e9a68322a2b76145264b4cfd2a16afb78
khurraim <khurraim.bcs46.m@gmail.com>
2024-01-19 19:23:10 +0500

    Column Set

--- pages/Contact.js
+++ pages/Contact.js
@@ -100,7 +100,7 @@ function Contact() {
       <main className="main-wrapper innrerPages-waper bg-white">
         <div className="container-lg inner-product">
           <div className="row px-lg-5">
-            <div className="col-lg-7 mx-auto">
+            <div className="col-lg-7 col-md-9 mx-auto">
               <div className="product-card">
                 <div className="img-wraper">
                   <img className="img-fluid" src="images/about.png" />
